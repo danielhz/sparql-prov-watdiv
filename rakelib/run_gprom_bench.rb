feec10ccea8b859ff350e3ad41e0a4599f040f94
Ubuntu <ubuntu@bench-manager.lxd>
2021-05-24 16:00:40 +0000

    Update query runner

--- rakelib/run_gprom_bench.rb
+++ rakelib/run_gprom_bench.rb
@@ -3,51 +3,41 @@
 ENGINE = 'sqlite'
 
 %w{sqlite}.each do |engine|
-  %w{spo pso 6idx vp}.each do |idx|
-    %w{B P}.each do |mode|
-      schema = case idx
-               when 'spo'
-                 'quads'
-               when 'pso'
-                 'quads'
-               when '6idx'
-                 'quads'
-               when 'vp'
-                 'vp'
-               else
-                 raise "Unsuported idx #{idx}"
-               end
-      puts "queries/100M/*/#{schema}/#{mode}/00.sql"
-      puts idx
+  %w{vp 6idx pso spo}.each do |idx|
+    # %w{C1 C2 C3 F1 F2 F3 F4 F5 L1 L2 L3 L4 L5 S1 S2 S3 S4 S5 S6 S7}.each do |template|
+    %w{C2 F3 C3 F4}.each do |template|
+      %w{B P}.each do |mode|
+        schema = case idx
+                 when 'spo'
+                   'quads'
+                 when 'pso'
+                   'quads'
+                 when '6idx'
+                   'quads'
+                 when 'vp'
+                   'vp'
+                 else
+                   raise "Unsuported idx #{idx}"
+                 end
+        
+        (1..5).each do |iteration|
 
-      (1..5).each do |iteration|
+          Dir["queries/100M/#{template}/#{schema}/#{mode}/*.sql"].to_a.sort.each do |query_file|
+            puts "Running #{query_file} on ENGINE=#{ENGINE} IDX=#{idx} ITERATION=#{iteration}"
+            query_name = query_file.sub('queries/', '').gsub('/', '-')
+            answers_name = "sqlite-#{query_name.sub(/.sql$/, '.txt')}".
+                             sub('quads', idx).sub(/.txt$/, "-#{iteration}.txt")
+            # puts "Answers name: #{answers_name}"
+            # puts "Query name: #{query_name}"
+            
+            system "lxc file push #{query_file} gprom/home/ubuntu/queries/#{query_name}"
+            system "lxc exec gprom -- su - ubuntu -c 'gprom -queryFile queries/#{query_name} " +
+                   "-backend sqlite -db watdiv-100M-#{idx}.db -timing > answers/#{answers_name}'"
+            system "lxc file pull gprom/home/ubuntu/answers/#{answers_name} answers/#{answers_name}"
+          end      
+        end
 
-        Dir["queries/100M/*/#{schema}/#{mode}/*.sql"].sort.reject do |query_file|
-          /C1/ === query_file or
-            /C2/ === query_file or
-            #   /F1/ === query_file or /F2/ === query_file or
-            /F3/ === query_file or
-            # /F5/ === query_file or
-            #   /L1/ === query_file or /L2/ === query_file or /L3/ === query_file or /L4/ === query_file or /L5/ === query_file or
-            #   /S1/ === query_file or /S2/ === query_file or /S3/ === query_file or
-            /S4/ === query_file or
-            /C3/ === query_file or
-            /F4/ === query_file
-        end.each do |query_file|
-          puts "Running #{query_file} on ENGINE=#{ENGINE} IDX=#{idx} ITERATION=#{iteration}"
-          query_name = query_file.sub('queries/', '').gsub('/', '-')
-          answers_name = "sqlite-#{query_name.sub(/.sql$/, '.txt')}".
-                           sub('quads', idx).sub(/.txt$/, "-#{iteration}.txt")
-          # puts "Answers name: #{answers_name}"
-          # puts "Query name: #{query_name}"
-          
-          system "lxc file push #{query_file} gprom/home/ubuntu/queries/#{query_name}"
-          system "lxc exec gprom -- su - ubuntu -c 'gprom -queryFile queries/#{query_name} " +
-                 "-backend sqlite -db watdiv-100M-#{idx}.db -timing > answers/#{answers_name}'"
-          system "lxc file pull gprom/home/ubuntu/answers/#{answers_name} answers/#{answers_name}"
-        end      
       end
-
     end
   end
 end
