2c4d8645c26a92c399f3e16eec4f618b2239a099
Ubuntu <ubuntu@bench-manager.lxd>
2021-05-24 08:01:49 +0000

    Add results for GPromSqlite

---
+++ rakelib/parse_gprom_results.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+
+def parse_line(line)
+  row = line.split("\s")
+  file_name = row[0].split('-')
+  {
+    query_name: file_name[5],
+    repetition: file_name[6].split('.').first,
+    time: row[4]
+  }
+end
+
+%w{100M}.each do |size|
+  %w{sqlite}.each do |engine|
+    %w{C1 C2 C3 F1 F2 F3 F4 F5  L1 L2 L3 L4 L5 S1 S2 S3 S4 S5 S6 S7}.each do |template|
+      %w{spo pso 6idx vp}.each do |idx|
+        %w{B P}.each do |mode|
+          schema = case idx
+                   when 'spo'
+                     'quads'
+                   when 'pso'
+                     'quads'
+                   when '6idx'
+                     'quads'
+                   when 'vp'
+                     'vp'
+                   else
+                     raise "Unsuported idx #{idx}"
+                   end
+
+          results = "results/#{engine}-#{size}-#{template}-#{idx}-#{mode}.csv"
+          puts "Creating #{results}"
+
+          if Dir["answers/*#{template}-#{idx}-#{mode}-*-*.txt"].size > 0
+            CSV.open(results, 'w') do |csv|
+              csv << %w{engine size template scheme mode query_id repetition time status}
+              
+              `grep 'timer: TOTAL' answers/*#{template}-#{idx}-#{mode}-*-*.txt | less`.each_line do |line|
+                result = parse_line(line)
+                csv << [engine, size, template, idx, mode, result[:query_name], result[:repetition], result[:time], 200]
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- rakelib/run_gprom_bench.rb
+++ rakelib/run_gprom_bench.rb
@@ -1,14 +1,56 @@
 #!/usr/bin/env ruby
 
-Dir['queries/100M/*/SQL/00.sql'].sort.reject do |query_file|
-  /C1/ === query_file or /C2/ === query_file or /C3/ === query_file or /F4/ === query_file
-end.each do |query_file|
-  puts "Running #{query_file}"
-  query_name = query_file.sub('queries/', '').gsub('/', '-')
-  answers_name = "sqlite-psog-#{query_name.sub(/.sql$/, '.txt')}"
-  
-  system "lxc file push #{query_file} gprom/home/ubuntu/#{query_name}"
-  system "lxc exec gprom -- su - ubuntu -c 'gprom -queryFile #{query_name} " +
-         "-backend sqlite -db watdiv-100M-psog.db -timing > answers/#{answers_name}'"
-  system "lxc file pull gprom/home/ubuntu/answers/#{answers_name} answers/#{answers_name}"
+ENGINE = 'sqlite'
+
+%w{sqlite}.each do |engine|
+  %w{spo pso 6idx vp}.each do |idx|
+    %w{B P}.each do |mode|
+      schema = case idx
+               when 'spo'
+                 'quads'
+               when 'pso'
+                 'quads'
+               when '6idx'
+                 'quads'
+               when 'vp'
+                 'vp'
+               else
+                 raise "Unsuported idx #{idx}"
+               end
+      puts "queries/100M/*/#{schema}/#{mode}/00.sql"
+      puts idx
+
+      (1..5).each do |iteration|
+
+        Dir["queries/100M/*/#{schema}/#{mode}/*.sql"].sort.reject do |query_file|
+          /C1/ === query_file or
+            /C2/ === query_file or
+            #   /F1/ === query_file or /F2/ === query_file or
+            /F3/ === query_file or
+            # /F5/ === query_file or
+            #   /L1/ === query_file or /L2/ === query_file or /L3/ === query_file or /L4/ === query_file or /L5/ === query_file or
+            #   /S1/ === query_file or /S2/ === query_file or /S3/ === query_file or
+            /S4/ === query_file or
+            /C3/ === query_file or
+            /F4/ === query_file
+        end.each do |query_file|
+          puts "Running #{query_file} on ENGINE=#{ENGINE} IDX=#{idx} ITERATION=#{iteration}"
+          query_name = query_file.sub('queries/', '').gsub('/', '-')
+          answers_name = "sqlite-#{query_name.sub(/.sql$/, '.txt')}".
+                           sub('quads', idx).sub(/.txt$/, "-#{iteration}.txt")
+          # puts "Answers name: #{answers_name}"
+          # puts "Query name: #{query_name}"
+          
+          system "lxc file push #{query_file} gprom/home/ubuntu/queries/#{query_name}"
+          system "lxc exec gprom -- su - ubuntu -c 'gprom -queryFile queries/#{query_name} " +
+                 "-backend sqlite -db watdiv-100M-#{idx}.db -timing > answers/#{answers_name}'"
+          system "lxc file pull gprom/home/ubuntu/answers/#{answers_name} answers/#{answers_name}"
+        end      
+      end
+
+    end
+  end
 end
+
+
+
